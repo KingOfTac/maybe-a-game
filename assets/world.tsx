431308c75ffb5b3dba3c76a0075124542a5fd5c9
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2025-09-24 17:12:26 -0600

    WIP sprites

--- assets/world.tsx
+++ assets/world.tsx
@@ -1,2 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="world" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.11.2" name="sprites" tilewidth="32" tileheight="32" tilecount="336" columns="21">
+ <image source="sprites.ase" width="674" height="543"/>
+</tileset>
